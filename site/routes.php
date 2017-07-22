62380a0be526cfba89eeacb0be7c2ccb59f7c500
Joseph Thomas <info@standard-quality.biz>
2017-07-21 18:35:38 -0700

    Updated routes

--- site/routes.php
+++ site/routes.php
@@ -4,21 +4,21 @@ function fetchPage($uri, $children = false) {
   return kirby()->site()->pages()->findByURI($uri)->getPublicContent($children);
 }
 
-function fetchSection($section) {
-  $pages = array();
-  foreach(kirby()->site()->pages()->find($section) as $page) {
-    if ($page) array_push($pages, $page);
-  }
-  return $page;
-}
-
-function fetchChoreographers() {
-  $choreographers = array();
-  foreach (kirby()->site()->pages()->find('choreographers')->children()->visible() as $page) {
-    array_push($choreographers, $page->getPublicContent());
-  }
-  return $choreographers;
-}
+// function fetchSection($section) {
+//   $pages = array();
+//   foreach(kirby()->site()->pages()->find($section) as $page) {
+//     if ($page) array_push($pages, $page);
+//   }
+//   return $page;
+// }
+//
+// function fetchChoreographers() {
+//   $choreographers = array();
+//   foreach (kirby()->site()->pages()->find('choreographers')->children()->visible() as $page) {
+//     array_push($choreographers, $page->getPublicContent());
+//   }
+//   return $choreographers;
+// }
 
 c::set('routes', array(
   array(
@@ -27,7 +27,7 @@ c::set('routes', array(
     'action' => function() {
       $content = new StdClass();
       $content->home = fetchPage('/home');
-      $content->choreographers = fetchChoreographers();
+      $content->choreographers = fetchPage('/choreographers', 1);
       $content->classes = fetchPage('/classes', 2);
       $content->community = fetchPage('/community', 1);
       return response::json(json_encode($content));
@@ -42,6 +42,7 @@ c::set('routes', array(
       $content->choreographers = fetchChoreographers();
       return response::json(json_encode($content));
     }
-  ),))
+  ),    
+))
 
- ?>
+?>
