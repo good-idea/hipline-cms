34fb6702d7af78389c73337cc8b2a663eeab52c7
Joseph Thomas <info@standard-quality.biz>
2018-01-04 16:41:48 -0800

    Makes choreographers empty array if empty

--- site/models/class.php
+++ site/models/class.php
@@ -12,6 +12,9 @@ class ClassPage extends Page {
 			}
 		}
 		$content['program'] = $this->parent()->getPublicContent();
+		if (($content['choreographers']) === '') {
+			$content['choreographers'] = [];
+		}
 		unset($content['mbodescription']);
 		return $content;
 	}
