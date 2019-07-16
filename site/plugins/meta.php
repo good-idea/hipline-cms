e91819408e2f6d41aa9aa457084fa3a2b86ae7bc
Joseph Thomas <joseph@good-idea.studio>
2019-07-16 12:10:49 -0700

    fix: fix SEO parsing

--- site/plugins/meta.php
+++ site/plugins/meta.php
@@ -5,19 +5,19 @@ function meta($page) {
 	$site = kirby()->site();
 	$homepage = $site->pages()->find('home');
 
-	$homepageImage = $homepage->og_image();
+	$homepageImage = $homepage->seo_image();
 	$defaultImage = ($homepageImage) ? $homepage->images()->find($homepageImage) : '';
 
-	$homepageDescription = $homepage->og_description();
+	$homepageDescription = $homepage->seo_description();
 	$defaultDescription = ($homepageDescription) ? (string)$homepageDescription : '';
 
 	$page = ($page) ? $page : $homepage;
 	$title = ($page->isHomepage()) ? $site->title() : $site->title() . " | " . $page->title();
 
-	$description = (strlen((string)$page->og_description()) > 0) ? (string)$page->og_description() : $defaultDescription;
+	$description = (strlen((string)$page->seo_description()) > 0) ? (string)$page->seo_description() : $defaultDescription;
 	$description = strip_tags(kirbytext($description));
 
-	$pageImage = $page->images()->find($page->og_image());
+	$pageImage = $page->images()->find($page->seo_image());
 	$image = ($pageImage) ? $pageImage : $defaultImage;
 	// $image = thumb($image, array('width', 1200), false);
 	$meta = new StdClass();
