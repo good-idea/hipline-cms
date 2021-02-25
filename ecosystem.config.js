c5acb5efc53db05aa3791272fe953658caef06cd
Joseph Thomas <joseph@good-idea.studio>
2021-02-25 14:50:59 -0800

    set up pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	deploy: {
+		production: {
+			user: 'appolonia',
+			host: '165.227.213.118',
+			ref: 'origin/main',
+			repo: 'git@github.com:good-idea/hipline-cms',
+			path: '/home/appolonia/panel.myhipline.com',
+		},
+	},
+}
